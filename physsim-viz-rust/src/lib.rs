b80294cece5473c28055a37bcddfbbc5014a7ce3
Tomasz Kawalec <tkawalec369@gmail.com>
2024-12-22 21:37:25 +0100

    Simulate and draw rigid cuboid

--- physsim-viz-rust/src/lib.rs
+++ physsim-viz-rust/src/lib.rs
@@ -17,6 +17,22 @@ pub fn greet() {
 
 struct RunnerState {
     counter: i32,
+    rigid_body: physsim::RigidBody<f32>,
+}
+
+impl RunnerState {
+    fn new() -> Self {
+        Self {
+            counter: 0,
+            rigid_body: physsim::RigidBody {
+                pos: nalgebra::Vector3::new(0.0, 0.0, 0.0),
+                lin_vel: nalgebra::Vector3::new(0.01, -0.02, 0.0),
+                rot_mat: nalgebra::Matrix3::identity(),
+                ang_mom: nalgebra::Vector3::new(0.5, -0.1, 0.3),
+                inv_ine: nalgebra::Matrix3::new(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0),
+            },
+        }
+    }
 }
 
 #[wasm_bindgen]
@@ -50,12 +66,13 @@ impl Runner {
             web_sys::WebGl2RenderingContext::VERTEX_SHADER,
             r##"#version 300 es
 
-        in vec4 position;
+            in vec3 position;
+            uniform mat4 projection;
 
-        void main() {
-            gl_Position = position;
-        }
-        "##,
+            void main() {
+                gl_Position = projection * vec4(position, 1.0);
+            }
+            "##,
         )?;
 
         let frag_shader = compile_shader(
@@ -63,13 +80,13 @@ impl Runner {
             web_sys::WebGl2RenderingContext::FRAGMENT_SHADER,
             r##"#version 300 es
 
-        precision highp float;
-        out vec4 outColor;
+            precision highp float;
+            out vec4 outColor;
 
-        void main() {
-            outColor = vec4(1, 1, 1, 1);
-        }
-        "##,
+            void main() {
+                outColor = vec4(1, 1, 1, 1);
+            }
+            "##,
         )?;
 
         let program = link_program(&ctx, &vert_shader, &frag_shader)?;
@@ -96,10 +113,10 @@ impl Runner {
 
         ctx.bind_vertex_array(Some(&vao));
 
-        let mut runner_state = RunnerState { counter: 0 };
+        let mut runner_state = RunnerState::new();
 
         let closure = Closure::new(move || {
-            draw(&ctx, &vbo, &vao, &mut runner_state);
+            draw(&ctx, &vbo, &vao, &program, &mut runner_state);
         });
         let token = window.set_interval_with_callback_and_timeout_and_arguments_0(
             closure.as_ref().unchecked_ref(),
@@ -170,28 +187,139 @@ fn link_program(
     }
 }
 
+fn cuboid_to_vertices(vertices: &mut Vec<f32>, rigid_body: &physsim::RigidBody<f32>) {
+    fn add_vert(vertices: &mut Vec<f32>, v: nalgebra::Vector3<f32>) {
+        vertices.push(v.x);
+        vertices.push(v.y);
+        vertices.push(v.z);
+    }
+
+    let v1 = rigid_body.rot_mat * nalgebra::Vector3::new(-0.5, -0.5, -0.5) + rigid_body.pos;
+    let v2 = rigid_body.rot_mat * nalgebra::Vector3::new(-0.5, -0.5, 0.5) + rigid_body.pos;
+    let v3 = rigid_body.rot_mat * nalgebra::Vector3::new(-0.5, 0.5, -0.5) + rigid_body.pos;
+    let v4 = rigid_body.rot_mat * nalgebra::Vector3::new(-0.5, 0.5, 0.5) + rigid_body.pos;
+    let v5 = rigid_body.rot_mat * nalgebra::Vector3::new(0.5, -0.5, -0.5) + rigid_body.pos;
+    let v6 = rigid_body.rot_mat * nalgebra::Vector3::new(0.5, -0.5, 0.5) + rigid_body.pos;
+    let v7 = rigid_body.rot_mat * nalgebra::Vector3::new(0.5, 0.5, -0.5) + rigid_body.pos;
+    let v8 = rigid_body.rot_mat * nalgebra::Vector3::new(0.5, 0.5, 0.5) + rigid_body.pos;
+
+    //F1
+    add_vert(vertices, v1);
+    add_vert(vertices, v2);
+    add_vert(vertices, v3);
+
+    //F2
+    add_vert(vertices, v2);
+    add_vert(vertices, v3);
+    add_vert(vertices, v4);
+
+    //F3
+    add_vert(vertices, v1);
+    add_vert(vertices, v3);
+    add_vert(vertices, v7);
+
+    //F4
+    add_vert(vertices, v1);
+    add_vert(vertices, v5);
+    add_vert(vertices, v7);
+
+    //F5
+    add_vert(vertices, v1);
+    add_vert(vertices, v2);
+    add_vert(vertices, v6);
+
+    //F6
+    add_vert(vertices, v1);
+    add_vert(vertices, v5);
+    add_vert(vertices, v6);
+
+    //F7
+    add_vert(vertices, v5);
+    add_vert(vertices, v6);
+    add_vert(vertices, v7);
+
+    //F8
+    add_vert(vertices, v6);
+    add_vert(vertices, v7);
+    add_vert(vertices, v8);
+
+    //F9
+    add_vert(vertices, v2);
+    add_vert(vertices, v4);
+    add_vert(vertices, v8);
+
+    //F10
+    add_vert(vertices, v2);
+    add_vert(vertices, v6);
+    add_vert(vertices, v8);
+
+    //F11
+    add_vert(vertices, v3);
+    add_vert(vertices, v4);
+    add_vert(vertices, v8);
+
+    //F12
+    add_vert(vertices, v3);
+    add_vert(vertices, v7);
+    add_vert(vertices, v8);
+}
+
 fn draw(
     ctx: &web_sys::WebGl2RenderingContext,
     vbo: &web_sys::WebGlBuffer,
     vao: &web_sys::WebGlVertexArrayObject,
+    program: &web_sys::WebGlProgram,
     state: &mut RunnerState,
 ) {
     web_sys::console::log_1(&"Drawing...".into());
 
-    let vertices: [f32; 9] = [
-        -0.7,
-        -0.7,
-        0.0,
-        0.7,
-        -0.7,
-        0.0,
-        -0.8 + (state.counter as f32) * 0.002,
-        0.7,
-        0.0,
-    ];
+    web_sys::console::log_1(&format!("{:?}", state.rigid_body).into());
+    web_sys::console::log_1(&format!("{:?}", state.rigid_body.rot_mat.determinant()).into());
+
+    //let vertices: [f32; 9] = [
+    //    -0.7,
+    //    -0.7,
+    //    0.0,
+    //    0.7,
+    //    -0.7,
+    //    0.0,
+    //    -0.8 + (state.counter as f32) * 0.002,
+    //    0.7,
+    //    0.0,
+    //];
+
+    let mut vertices: Vec<f32> = Vec::new();
+    cuboid_to_vertices(&mut vertices, &state.rigid_body);
 
     ctx.bind_buffer(web_sys::WebGl2RenderingContext::ARRAY_BUFFER, Some(&vbo));
     ctx.bind_vertex_array(Some(&vao));
+
+    let aspect = 1.333;
+    let fovy: f32 = 75.0 * std::f32::consts::PI / 180.0;
+    let tan_half_fovy = (fovy / 2.0).tan();
+    let z_far = 1000.0;
+    let z_near = 0.01;
+
+    //let mut proj_mat = nalgebra::Matrix4::<f32>::identity();
+    //proj_mat.data.0[0][0] = 1.0 / (aspect * tan_half_fovy);
+    //proj_mat.data.0[1][1] = 1.0 / tan_half_fovy;
+    //proj_mat.data.0[2][2] = z_far / (z_far - z_near);
+    //proj_mat.data.0[2][3] = 1.0;
+    //proj_mat.data.0[3][2] = -(z_far * z_near) / (z_far - z_near);
+
+    let persp = nalgebra::Perspective3::new(aspect, fovy, z_near, z_far);
+
+    let translation = nalgebra::Translation3::<f32>::new(0.0, 0.0, -5.0);
+    let proj_mat = persp.as_matrix() * translation.to_homogeneous();
+
+    let proj_uni_loc = ctx
+        .get_uniform_location(program, "projection")
+        .expect("Uniform projection not found");
+    ctx.uniform_matrix4fv_with_f32_array(
+        Some(&proj_uni_loc),
+        false,
+        &proj_mat.data.0.as_flattened(),
+    );
 
     unsafe {
         let vertices_view = js_sys::Float32Array::view(&vertices);
@@ -211,4 +339,5 @@ fn draw(
     ctx.draw_arrays(web_sys::WebGl2RenderingContext::TRIANGLES, 0, vert_count);
 
     state.counter += 1;
+    state.rigid_body.step_sim(0.1);
 }
